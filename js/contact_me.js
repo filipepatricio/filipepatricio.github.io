d5394fee3c31f9bb701fe1170c541e6b4fcd1357
Filipe Patricio <facpatricio@gmail.com>
2018-06-15 16:17:02 +0100

    Update formfree url

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "//formspree.io/facpatricio@gmail.com",
+        url: "https://formspree.io/facpatricio@gmail.com",
         type: "POST",
         data: {
           name: name,
